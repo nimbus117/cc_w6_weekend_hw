c2499345aaadfdd70c9da06c2aecc0d5d2925142
nimbus117 <mail@nimbus117.co.uk>
2018-09-07 23:33:37 +0100

    refactored addTextAndAppend to include creating the element, renamed to createAndAppend, also takes the element to append to as an arg.

--- js/app.js
+++ js/app.js
@@ -1,45 +1,51 @@
 // wait until all DOM content is loaded before form/button actions are available
 document.addEventListener('DOMContentLoaded', () => {
+
   // add form submit event listener
   const form = selector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
+
   // add delete button event listener
   const deleteButton = selector('#deleteButton');
   deleteButton.addEventListener('click', handleDeleteButton);
 })
 
 // function called by form submit event listener
-const handleFormSubmit = function(e) {
+const handleFormSubmit = function (e) {
+
   // prevent default form behaviour
   e.preventDefault();
-  // helper function - set element text content and append to newTask
-  const addTextAndAppend = (element, input) => {
-    element.textContent = this[input].value;
-    newTask.appendChild(element);
+
+  // helper function - create element, set text content and append to newTask
+  const createAndAppend = (elementType, content, parent) => {
+    const element = creator(elementType);
+    element.textContent = this[content].value;
+    parent.appendChild(element);
   }
+
   // create li element 'newTask'
   const newTask = creator('li');
   // add html classes to newTask
   newTask.classList.add('flex-item');
   newTask.classList.add(this.priority.value);
+
   // create category element, set text content and append to newTask
-  const taskCategory = creator('h2');
-  addTextAndAppend(taskCategory, 'category')
+  createAndAppend('h2', 'category', newTask);
   // create dueDate element, set text content and append to newTask
-  const taskDueDate = creator('h5');
-  addTextAndAppend(taskDueDate, 'dueDate')
+  createAndAppend('h5', 'dueDate', newTask);
   // create task (details) element, set text content and append to newTask
-  const task = creator('p');
-  addTextAndAppend(task, 'task')
-  // append the newTask to the task list ul element
+  createAndAppend('p', 'task', newTask);
+
+  // prepend newTask to the task list ul element
   const taskList = selector('#task-list');
   taskList.insertBefore(newTask, taskList.firstChild);
+
   // reset form after submission
   this.reset();
 };
 
 // function called by deleteButton click event listener
-const handleDeleteButton = function (){
+const handleDeleteButton = function () {
   const taskList = selector('#task-list');
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
