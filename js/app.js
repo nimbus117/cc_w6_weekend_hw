9337e6cdb5060f4de7d69e06297e6ad1675fe564
nimbus117 <mail@nimbus117.co.uk>
2018-09-07 12:29:14 +0100

    added js to handle form submit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('test javascript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+})
+
+const handleFormSubmit = function(e) {
+  e.preventDefault();
+  // console.log('test form submit');
+
+  const newTask = document.createElement('li');
+  newTask.classList.add('flex-item');
+
+  const task = document.createElement('h2');
+  task.textContent = this.task.value;
+  newTask.appendChild(task);
+
+  const taskDueDate = document.createElement('h4');
+  taskDueDate.textContent = this.dueDate.value;
+  newTask.appendChild(taskDueDate);
+
+  const taskCategory = document.createElement('p');
+  taskCategory.textContent = this.category.value;
+  newTask.appendChild(taskCategory);
+
+  const taskList = document.querySelector('#task-list');
+  taskList.appendChild(newTask);
+
+  const priority = this.priority.value
+  newTask.classList.add(priority);
+
+  this.reset();
+};
